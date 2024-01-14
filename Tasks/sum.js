7e16ba4b70c7d63f6d7c0adf55c5d222c9d1aa54
Space6188 <barabashnazarx@gmail.com>
2024-01-15 02:07:11 +0300

    Fix bug inside Sum function, problem with protection and arr not returning while protection was active

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,7 +1,11 @@
 // Sum all numbers from an array
 sum = (arr) =>
   arr.reduce((acc, value) => {
-    if (typeof value === "number") acc + value;
+    if (typeof value === "number") {
+      return acc + value;
+    } else {
+      return acc;
+    }
   });
 
 module.exports = sum;
