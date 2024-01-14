ae4d7c260985761905e9902265ba064ac27ec619
Space6188 <barabashnazarx@gmail.com>
2024-01-15 01:31:30 +0300

    Array without duplicates using Set

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,5 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
-  }
-};
+duplicate = (arr) => [...new Set(arr)];
 
 module.exports = duplicate;
