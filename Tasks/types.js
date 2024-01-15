307e07ff4db5fb9ba7b8587f18cedcbbde196d27
Space6188 <barabashnazarx@gmail.com>
2024-01-15 03:15:59 +0300

    Remove return

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,10 +1,9 @@
 // Count types in an array
 const defaultVL = { number: 0, boolean: 0, string: 0 };
-types_ = (arr) => {
-  return arr.reduce((acc, value) => {
+types_ = (arr) =>
+  arr.reduce((acc, value) => {
     const type = typeof value;
     acc[type] = (acc[type] || 0) + 1;
     return acc;
   }, defaultVL);
-};
 module.exports = types_;
