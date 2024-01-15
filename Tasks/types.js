1d583e8dc445de958e37772893bf10d013e1a519
Space6188 <barabashnazarx@gmail.com>
2024-01-15 03:13:16 +0300

    Add default values in object

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,11 +1,10 @@
 // Count types in an array
-
+const defaultVL = { number: 0, boolean: 0, string: 0 };
 types_ = (arr) => {
   return arr.reduce((acc, value) => {
     const type = typeof value;
     acc[type] = (acc[type] || 0) + 1;
     return acc;
-  }, {});
+  }, defaultVL);
 };
-
 module.exports = types_;
